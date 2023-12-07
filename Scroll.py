678db16ab032cf3d0dcb79a80c39f4e35f026197
foadmirzaahmadi <foadmirzaahmadi@outlook.com>
2023-12-07 09:31:35 +0330

    session11

--- Scroll.py
+++ Scroll.py
@@ -9,7 +9,46 @@ driver.maximize_window()
 driver.implicitly_wait(3)
 driver.get('https://www.imdb.com/chart/top/')
 
-element = driver.find_element('link text','200. Sherlock Jr.')
-print(element)
-driver.execute_script("arguments[0].scrollIntoView();",element)
-sleep(5)
+# element = driver.find_element('link text','200. Sherlock Jr.')
+# print(element)
+# driver.execute_script("arguments[0].scrollIntoView();",element)
+# sleep(5)
+
+
+
+# def scroll_to_find_element(locator,pixel):
+#     for i in range(10):
+#         try:
+#             driver.find_element(locator[0],locator[1])
+#             return True
+#         except:
+#             driver.execute_script(f"window.scrollBy(0,{str(pixel)})")
+#     return False
+#
+# driver.get('https://www.imdb.com/chart/top/')
+# result = scroll_to_find_element(['link text','168. Three Billboards Outside Ebbing, Missouri'],3400)
+# print(result)
+
+
+
+
+# def scroll_to_find_element(locator):
+#     for i in range(10):
+#         try:
+#             driver.find_element(locator[0],locator[1])
+#             return True
+#         except:
+#             driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+#     return False
+#
+# driver.get('https://www.imdb.com/chart/top/')
+# result = scroll_to_find_element(['link text','rerfrfd'])
+# print(result)
+
+# page_el= driver.find_element('tag name','html')
+# acions.send_keys_to_element(page_el,Keys.END).perform()
+# sleep(5)
+
+# element = driver.find_element('link text','205. Mary and Max')
+# element.location_once_scrolled_into_view
+# sleep(5)
