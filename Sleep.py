171340acda8ce5f293f4534a2c7597e03ea2582b
foadmirzaahmadi <foadmirzaahmadi@outlook.com>
2023-12-24 16:47:59 +0330

    session 12 time 25:00

--- Sleep.py
+++ Sleep.py
@@ -7,23 +7,42 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.implicitly_wait(1)
 
-def wait_until_element_has_an_attribute(element_selector,element_locator,attribute,attribute_value,timeout=5,exact=True):
-    for i in range(timeout * 5):
-        try:
-            element = driver.find_element(element_selector,element_locator)
-            if exact:
-                assert element.get_attribute(attribute) == attribute_value
-            else:
-                assert attribute_value in element.get_attribute(attribute)
-            return
-        except:
-            sleep(0.2)
-    raise Exception("element attribute is not:"+str(attribute))
+# def wait_until_element_has_an_attribute(element_selector,element_locator,attribute,attribute_value,timeout=5,exact=True):
+#     for i in range(timeout * 5):
+#         try:
+#             element = driver.find_element(element_selector,element_locator)
+#             if exact:
+#                 assert element.get_attribute(attribute) == attribute_value
+#             else:
+#                 assert attribute_value in element.get_attribute(attribute)
+#             return
+#         except:
+#             sleep(0.2)
+#     raise Exception("element attribute is not:" + str(attribute))
+#
+#
+# def wait_until_element_has_not_an_attribute(element_selector,element_locator,attribute,attribute_value,timeout=5,exact=True):
+#     for i in range(timeout * 5):
+#         try:
+#             element = driver.find_element(element_selector,element_locator)
+#             if exact:
+#                 assert element.get_attribute(attribute) != attribute_value
+#             else:
+#                 assert attribute_value not in element.get_attribute(attribute)
+#             return
+#         except:
+#             sleep(0.2)
+#     raise Exception("element attribute is not:"+str(attribute))
+#
+#
+# wait_until_element_has_an_attribute(By.ID,'enabled_target','class','danger',exact=False)
+# wait_until_element_has_not_an_attribute(By.ID,'enabled_target','class','success',exact=False)
+# trigger.click()
+# wait_until_element_has_an_attribute(By.ID,'enabled_target','class','success',exact=False)
+# wait_until_element_has_not_an_attribute(By.ID,'enabled_target','class','danger',exact=False)
+
 
 driver.get('https://play1.automationcamp.ir/expected_conditions.html')
 trigger= driver.find_element(By.ID,'enabled_trigger')
 trigger.location_once_scrolled_into_view
-trigger.click()
-wait_until_element_has_an_attribute(By.ID,'enabled_target','class','success',exact=False)
-
-
+driver.find_element(By.ID,)
