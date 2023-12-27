d0478bf82fca7fb584cc1477f7c2905598329533
foadmirzaahmadi <foadmirzaahmadi@outlook.com>
2023-12-27 17:44:43 +0330

    session 13 time 31:50

--- session 13.py
+++ session 13.py
@@ -3,22 +3,21 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.action_chains import ActionChains
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.implicitly_wait(1)
 alert = Alert(driver)
 actions = ActionChains(driver)
 # driver.get('https://the-internet.herokuapp.com/javascript_alerts')
-driver.get('https://material.angular.io/components/dialog/examples')
+driver.get('https://material.angular.io/components/snack-bar/examples')
 
-
-
-#Get_Text_alert
+# Get_Text_alert
 # driver.find_element(By.XPATH,"//button[text()='Click for JS Alert']").click()
 # print(alert.text)
 
 
-#Accept alert
+# Accept alert
 # driver.find_element(By.XPATH,"//button[text()='Click for JS Confirm']").click()
 # alert.accept()
 # alert_assert = driver.find_element(By.XPATH,"//*[text()='You clicked: Ok']")
@@ -26,7 +25,7 @@ driver.get('https://material.angular.io/components/dialog/examples')
 # assert "You clicked: Ok" in dom
 
 
-#Dismiss alert
+# Dismiss alert
 # driver.find_element(By.XPATH,"//button[text()='Click for JS Confirm']").click()
 # alert.dismiss()
 # alert_assert = driver.find_element(By.XPATH,"//*[text()='You clicked: Cancel']")
@@ -34,7 +33,7 @@ driver.get('https://material.angular.io/components/dialog/examples')
 # assert "You clicked: Cancel" in dom
 
 
-#send text
+# send text
 # driver.find_element(By.XPATH,"//button[text()='Click for JS Prompt']").click()
 # alert.send_keys("this is foad")
 # alert.accept()
@@ -42,16 +41,33 @@ driver.get('https://material.angular.io/components/dialog/examples')
 # assert "this is foad" in dom
 
 
-#dialog or popup
+# dialog or popup
 
-cdk= driver.find_element(By.XPATH,"//*[@class ='mdc-button__label' and text()='CDK']")
-offset = cdk.location
-driver.find_element(By.XPATH,"//*[text()='Ok, Got it']").click()
-driver.find_element(By.XPATH,"//span[text()='Open dialog']").click()
-driver.find_element(By.XPATH,"//h3[text()='Develop across all platforms']")
-driver.find_element(By.XPATH,"//span[text()='Install']")
-driver.find_element(By.XPATH,"//span[text()='Cancel']")
-actions.move_by_offset(offset['x'],offset['y']).pause(0.5).click().perform()
-driver.find_element(By.XPATH,"//span[text()='Open dialog']").click()
+# cdk= driver.find_element(By.XPATH,"//*[@class ='mdc-button__label' and text()='CDK']")
+# offset = cdk.location
+# driver.find_element(By.XPATH,"//*[text()='Ok, Got it']").click()
+# driver.find_element(By.XPATH,"//span[text()='Open dialog']").click()
+# driver.find_element(By.XPATH,"//h3[text()='Develop across all platforms']")
+# driver.find_element(By.XPATH,"//span[text()='Install']")
+# driver.find_element(By.XPATH,"//span[text()='Cancel']")
+# actions.move_by_offset(offset['x'],offset['y']).pause(0.5).click().perform()
+# driver.find_element(By.XPATH,"//span[text()='Open dialog']").click()
 
+#
+# snack bar
 
+input = driver.find_element(By.ID, "mat-input-4")
+input.clear()
+input.send_keys(1)
+driver.find_element(By.XPATH,
+                    "//*[@class ='mdc-button__label' and normalize-space(text())='Pizza party']//ancestor::button").click()
+
+# 1:
+driver.find_element(By.XPATH, "//*[@class='cdk-overlay-container']//*[contains(text(),'Pizza party')")
+
+# # 2:
+# driver.find_element(By.XPATH, "//*[@class='cdk-overlay-container']/following::*[contains(text(),'Pizza party')")
+#
+# #3:
+# dom =driver.page_source
+# print(dom)
